07bae3950847a638e184fb47f03c067e642bade4
DenisAb <denis_abubakirov@mail.ru>
2014-03-29 20:17:01 +0400

    add 18 t&t

---
+++ hw-1/task_18.s
@@ -0,0 +1,58 @@
+        .data			#while i >0 print * and i--
+ivar:
+        .space 4 
+
+        
+strforscan:
+        .string  "%d"
+strpr:
+        .string  "*"
+strend:
+	.string	"\n"
+
+	.text
+	.global main
+
+main:   pushl   %ebp			# prolog
+        movl    %esp,   %ebp
+
+	pushl 	$ivar
+        pushl   $strforscan
+        call    scanf
+        addl    $8,     %esp
+
+looop:	
+        movl    ivar,   %eax
+        cmpl    $0 ,   %eax            # compare ivar with 0
+        jne     next
+	je	end
+next:              
+        
+        pushl   $strpr
+        call    printf
+        addl    $4,     %esp
+	
+        movl    ivar,     %eax
+	decl 	%eax
+        movl    %eax,	ivar 	
+
+        movl    ivar,   %eax
+        cmpl    $0 ,   %eax            # compare ivar with 0
+        jne     looop            
+        
+
+
+end:
+
+        pushl   $strend
+        call    printf
+        addl    $4,     %esp
+
+
+
+
+        movl    $0 ,     %eax		# epilog
+        movl    %ebp,   %esp
+        popl    %ebp
+        ret
+
