e870337489206caf93cfcb6ac7f37b83eb891200
DenisAb <denis_abubakirov@mail.ru>
2014-03-31 17:55:34 +0400

    Add dir with factorial part of №4

---
+++ ex-1/task.1.s
@@ -0,0 +1,64 @@
+.data
+printf_format:
+	.string "%d\n"
+.text
+
+#* int factorial(int) */
+
+factorial:
+	pushl	 %ebp		#prolog
+	movl 	%esp, 	%ebp
+			#/*извлечь аргумент в %eax */
+	movl 	8(%ebp),%eax
+			#/* факториал 0 равен 1 */
+	cmpl $0, %eax
+	jne	not_zero
+
+	movl $1, %eax
+	jmp return
+
+not_zero:
+
+	#/* следующие 4 строки вычисляют выражение
+	#%eax = factorial(%eax - 1) */
+
+	decl 	%eax
+	pushl	%eax
+	call	factorial
+	addl	$4,	 %esp
+
+	#извлечь в %ebx аргумент и вычислить %eax = %eax * %ebx */
+
+	movl	8(%ebp),%ebx
+	null 	%ebx
+
+	#/* результат в паре %edx:%eax, но старшие 32 бита нужно
+	#отбросить,
+	#так как они не помещаются в int */
+
+return:
+	movl	%ebp, 	%esp	#epilog
+	popl	%ebp
+	ret
+
+.globl main
+main:
+	pushl %ebp		#prolog
+	movl %esp, %ebp
+
+	pushl $5
+	call factorial
+
+	pushl %eax
+	pushl $printf_format
+	call printf
+	
+	#/* стек можно не выравнивать, это будет сделано
+	#во время выполнения эпилога */
+
+	movl $0, %eax		/* завершить программу */
+
+	movl %ebp, %esp		#epilog
+	popl %ebp
+	ret
+
